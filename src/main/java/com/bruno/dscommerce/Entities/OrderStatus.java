19c322689a0c36d37533ae253d625f39896538b9
Bruno Ferrari <brunobferrari@gmail.com>
2023-05-26 10:06:19 -0300

    Order

---
+++ src/main/java/com/bruno/dscommerce/Entities/OrderStatus.java
@@ -0,0 +1,8 @@
+package com.bruno.dscommerce.Entities;
+
+public enum OrderStatus {
+	
+	WAITING_PAYMENT, PAID, SHIPPED, DELIVERED, CANCELED
+
+}
+
--- src/main/java/com/bruno/dscommerce/Entities/User.java
+++ src/main/java/com/bruno/dscommerce/Entities/User.java
@@ -1,11 +1,14 @@
 package com.bruno.dscommerce.Entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -14,12 +17,16 @@ public class User {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private long id;
+	private Long id;
 	private String name;
 	private String email;
 	private String phone;
 	private LocalDate birthDate;
 	private String password;
+	
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -80,6 +87,10 @@ public class User {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	
