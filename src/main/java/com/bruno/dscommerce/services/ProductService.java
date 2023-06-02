3383e67a7cef8deb039dcbfa5280cdba907503e3
Bruno Ferrari <brunobferrari@gmail.com>
2023-06-02 16:07:01 -0300

    Tratamento de excecoes

--- src/main/java/com/bruno/dscommerce/services/ProductService.java
+++ src/main/java/com/bruno/dscommerce/services/ProductService.java
@@ -3,14 +3,20 @@ package com.bruno.dscommerce.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.bruno.dscommerce.Entities.Product;
 import com.bruno.dscommerce.Repositories.ProductRepository;
 import com.bruno.dscommerce.dto.ProductDTO;
+import com.bruno.dscommerce.services.exceptions.ResourceNotFoundException;
+import com.bruno.dscommerce.services.exceptions.DatabaseException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class ProductService {
@@ -18,13 +24,13 @@ public class ProductService {
 	@Autowired
 	private ProductRepository repository;
 	
-	// buscar produto banco de dados
+	// buscar produto banco de dados / mensagem de erro
 	@Transactional(readOnly = true)
 	public ProductDTO findById(Long id) {
 		Optional<Product> result = repository.findById(id);
-		Product product = result.get();
-		ProductDTO dto = new ProductDTO(product);
-		return dto;
+		Product product = repository.findById(id).orElseThrow(
+				() -> new ResourceNotFoundException("Recurso não encontrado"));
+		return new ProductDTO(product);
 	}
 	
 	// listar produto
@@ -47,17 +53,34 @@ public class ProductService {
 	// atualizar produto por ID
 	@Transactional
 	public ProductDTO update(Long id, ProductDTO dto) {
-		Product entity = repository.getReferenceById(id);
-		copyDtoToEntity(dto, entity);
-		entity = repository.save(entity);
-		return new ProductDTO(entity);
+		// Atualizaçao de ID que nao existe
+		try {
+			Product entity = repository.getReferenceById(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ProductDTO(entity);
 	}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
+	}
+}
 	
 	// deletar Produto por ID
-	@Transactional
+	@Transactional(propagation = Propagation.SUPPORTS )
 	public void delete(Long id) {
-		repository.deleteById(id);
+		// deletar ID não existente / duas vezes o mesmo
+		if (!repository.existsById(id)) {
+			throw new ResourceNotFoundException("Recurso não encontrado");
 	}
+		try {
+	        	repository.deleteById(id);    		
+	}
+	    	catch (DataIntegrityViolationException e) {
+	        	throw new DatabaseException("Falha de integridade referencial");
+	        	
+	}
+	}
+	
 
 	private void copyDtoToEntity(ProductDTO dto, Product entity) {
 		entity.setName(dto.getName());
---
+++ src/main/java/com/bruno/dscommerce/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.bruno.dscommerce.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+	
+	public ResourceNotFoundException(String msg) {
+		super(msg);
+		
+		
+	}
+
+}
